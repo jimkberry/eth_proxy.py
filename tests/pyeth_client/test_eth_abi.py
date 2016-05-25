de1c97be7450de5fea62429cb67891f730b13944
Jim Berry <jim@bigfattail.com>
2016-05-25 11:37:11 -0400

    Added unimplmented pyeth_client tests

---
+++ tests/pyeth_client/test_eth_abi.py
@@ -0,0 +1,30 @@
+
+from eth_proxy.pyeth_client.eth_abi import *
+import pytest
+
+def test_encode_abi():
+    raise NotImplementedError
+
+def test_decode_abi():
+    raise NotImplementedError
+
+def test_encode_single():
+    raise NotImplementedError
+    
+def test_decint():
+    raise NotImplementedError
+
+def test_decode_single():
+    raise NotImplementedError
+
+def test_enc():
+    raise NotImplementedError
+
+def test_dec():
+    raise NotImplementedError
+
+def test_process_type():
+    raise NotImplementedError
+
+def test_get_size():
+    raise NotImplementedError
---
+++ tests/pyeth_client/test_eth_txdata.py
@@ -0,0 +1,6 @@
+
+from eth_proxy.pyeth_client.eth_txdata import *
+import pytest
+
+def test_eth_txdata():
+    raise NotImplementedError
