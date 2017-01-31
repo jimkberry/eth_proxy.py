83fee4f9f29c528d082d0e3c4d6de1051846ac7e
jim berry <jim@bigfattail.com>
2017-01-31 12:13:07 -0500

    Many changes to solc_caller (from poker)

--- eth_proxy/solc_caller.py
+++ eth_proxy/solc_caller.py
@@ -3,34 +3,32 @@
 #
 import logging as log
 import sys
-
 import time
 from types import ListType
-
 import json
 import re
 import subprocess
-
+import os
 
 class SolcCaller(object):
     '''
-    Just calls solc - assumes it is installed on the local machine
+    Just calls solc on a source file - assumes it is installed on the local machine
     '''
                    
     @staticmethod
-    def _call_solc(source_code, args):
+    def _call_solc(args):
         '''
         Just calls solc on the given source with the given args
         Note that "solc" is the first arg
-        '''
+        '''    
         p = subprocess.Popen(args, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-        (stdoutdata, stderrdata) = p.communicate(input=source_code)
+        (stdoutdata, stderrdata) = p.communicate()
         if p.returncode:
             raise RuntimeError('solc compilation failed')
         return stdoutdata
         
     @staticmethod
-    def compile_solidity(contract_code):
+    def compile_solidity(source_path):
         '''
         Changes to solc can to break pyethereum's solc wrapper, and we're not really
         using it to do much. So let's just do it here. Most of this is pretty much lifted from
@@ -44,11 +42,11 @@ class SolcCaller(object):
         version_info = subprocess.check_output(['solc', '--version'])
         match = re.search("^Version: ([0-9a-z.-]+)/", version_info, re.MULTILINE)
         vstr = match.group(1)    
-                  
+
         if vstr[:5] == '0.1.1':
-            args = ['solc', '--combined-json', 'binary']
+            args = ['solc', '--combined-json', 'binary', source_path]
         else:
-            args = ['solc', '--combined-json', 'bin']  # changed to "bin" in 0.1.2
+            args = ['solc', '--combined-json', 'bin', source_path]  # changed to "bin" in 0.1.2
                               
         #
         # This is for eth_contract metadata generation
@@ -61,7 +59,7 @@ class SolcCaller(object):
         # NOTE: I don't think this is true anymore - but's it's not broke...
         result = None
         try:
-            solc_results = SolcCaller._call_solc(contract_code, args)
+            solc_results = SolcCaller._call_solc(args)
             jdata = json.loads(solc_results)                           
                     
             # API changes with version. Sigh.
@@ -78,17 +76,18 @@ class SolcCaller(object):
         return result
         
     @staticmethod        
-    def generate_metadata(source_code):
+    def generate_metadata(source_path):
         '''
         This assumes a newer version of solc than _compile_solidity() assumed
         when it was written, so the arg-building process is not so weird.
         '''
-        args = ['solc', '--combined-json', 'abi,bin']
+        args = ['solc', '--combined-json', 'abi,bin', source_path]
         results = None
         try:
-            solc_results = SolcCaller._call_solc(source_code, args)
-            jdata = json.loads(solc_results)    
+            solc_results = SolcCaller._call_solc(args)                
+            jdata = json.loads(solc_results)
             # TODO: we can only handle a single contract per file
+            
             con1_name = jdata['contracts'].keys()[0]
             con1_data = jdata['contracts'].items()[0][1]
             # solc packs the ABI data into a single string
@@ -97,7 +96,7 @@ class SolcCaller(object):
             results = {'contract_name': con1_name, 'abi': abi_data, 'bin': bin_data}
         except:
             log.warning("generate_solc_metadata() failed: {0}".format(sys.exc_info()[0]))
-        
+
         return results
                 
 
