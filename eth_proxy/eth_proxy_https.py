83a2e37d76a3d1dad5c5ef9d0de7b64992a6d29a
jim berry <jim@bigfattail.com>
2017-01-31 12:17:31 -0500

    "requests" parameter changes (retries, timeout) from poker

--- eth_proxy/eth_proxy_https.py
+++ eth_proxy/eth_proxy_https.py
@@ -2,6 +2,8 @@
 #
 from eth_proxy_base import EthProxyBase
 import requests
+from requests.packages.urllib3.util.retry import Retry
+from requests.adapters import HTTPAdapter
 import json
 
 
@@ -10,14 +12,28 @@ class EthProxyHttps(EthProxyBase):
     EthProxy implemented using json-rpc over HTTPS
     
     See EthProxyBase for details.
+    
+    TODO: Too much copy/paste similarity between HTTP and HTTPS implementations
     '''
 
     def __init__(self, rpc_host, rpc_port):
         super(EthProxyHttps, self).__init__()        
         self.rpc_host = rpc_host
         self.rpc_port = rpc_port
+        self.timeout = 5.0
+        self.session = self._setup_session()
         
+        
+    def _setup_session(self):
+        s = requests.Session()
 
+        retries = Retry(total=5,
+                        backoff_factor=0.1,
+                        status_forcelist=[ 500, 502, 503, 504 ])
+
+        s.mount('https://', HTTPAdapter(max_retries=retries))        
+        return s
+        
     #
     # Internal stuff
     #
@@ -25,6 +41,7 @@ class EthProxyHttps(EthProxyBase):
         '''
         Actually execute the RPC call and field the result
         '''
+        #self.log.info("Eth Node request: {0}".format(method))        
         params = params or []
         data = json.dumps({
             'jsonrpc': '2.0',
@@ -32,14 +49,22 @@ class EthProxyHttps(EthProxyBase):
             'params': params,
             'id': _id
         })
-               
-               
-        response = requests.post("https://{0}:{1}".format(self.rpc_host, self.rpc_port), data=data).json()
+                         
+                         
+        headers = {'Content-Type': 'application/json'}
+        
+        url = "https://{0}:{1}".format(self.rpc_host, self.rpc_port)
+                         
+        response = self.session.post(url, timeout=self.timeout, headers=headers, data=data)    
 
-        if 'result' in response:
-            return response['result']
+        response.raise_for_status()
+  
+        json_resp = response.json()
+        #self.log.info("Eth Node response: {0}".format(json_resp))        
+        if 'result' in json_resp:
+            return json_resp['result']
         else:
-            raise RuntimeError('Error from RPC call. Returned payload: {0}'.format(response))
+            raise RuntimeError('Error from RPC call. Returned json payload: {0}'.format(json_resp))
 
 
 
