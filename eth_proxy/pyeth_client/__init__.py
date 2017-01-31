580140e9f9266d5b6a7161302a6c6ba021eea560
jim berry <jim@bigfattail.com>
2017-01-31 14:19:01 -0500

    maybe...

--- eth_proxy/pyeth_client/__init__.py
+++
@@ -1,8 +0,0 @@
-
-#
-# A subset of code from pyethereum needed by 
-# an Ethereum client application (not a node)
-# that wants to communicate via RPC (or ipc:
-# it's not in place but would be a simple 
-# addition) 
-#
