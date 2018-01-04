8fb06e739f41367a3116d779c1752ca73e271a87
jim berry <jim@bigfattail.com>
2018-01-04 14:39:30 -0500

    Updated common setup for func tests

--- func_tests/func_setups.py
+++ func_tests/func_setups.py
@@ -21,10 +21,12 @@ log.getLogger("urllib3").setLevel(log.WARNING)
 class FuncSetups(object):
     
     def __init__(self, host_ip=None):
-            #self.eth_host = 'https://ropsten.infura.io:443'
-            self.eth_host = 'https://infuranet.infura.io:443'
+            self.eth_host = 'https://ropsten.infura.io:443'
+            #self.eth_host = 'https://infuranet.infura.io:443'
+            #self.eth_host = 'https://rinkeby.infura.io:443'
             #eth_host = 'http://localhost:8545'
             #eth_host = 'ipc:/home/jim/cons-testnet-geth/data/geth.ipc'
+            
 
 
     def create_proxy(self, host_ip=None):
